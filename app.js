7d7c48c94d9e60aef36fa4ade43b9004a07adef9
Gabtaca <gabrieltaca117@gmail.com>
2024-02-28 20:44:20 -0500

    commit 2

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const colors = document.querySelectorAll('.color__id');
 const button = document.querySelector('#btn__randomizer');
 
 function generateColors() {
+    let gradientColors = [];
     colors.forEach((color, index) => {
         let hexCode = '#' + Math.random().toString(16).substring(2, 8);
         color.parentElement.style.borderTopColor = hexCode;
         color.innerHTML = hexCode;
-    }); 
+        gradientColors.push(hexCode);
+    });
+
+    let gradient = gradientColors.join(',');
+    document.body.style.background = `linear-gradient(45deg, ${gradient})`;
 }
 
 generateColors();
