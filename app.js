fce8153473fe3c2b5c10b15186d18d1abf16348a
Gabtaca <gabrieltaca117@gmail.com>
2024-03-01 09:23:03 -0500

    commit 3

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const colors = document.querySelectorAll('.color__id');
 const button = document.querySelector('#btn__randomizer');
+const hexagons = document.querySelectorAll('.color--hex');
+const hexToRgb = hex => {
+    const bigint = parseInt(hex.substring(1), 16);
+    const r = (bigint >> 16) & 255;
+    const g = (bigint >> 8) & 255;
+    const b = bigint & 255;
+    return `rgb(${r}, ${g}, ${b})`;
+};
 
 function generateColors() {
     let gradientColors = [];
@@ -17,3 +25,42 @@ function generateColors() {
 generateColors();
 
 button.addEventListener('click', generateColors);
+
+hexagons.forEach((hexagon, index) => {
+    const colorId = hexagon.querySelector('.color__id');
+
+    hexagon.addEventListener('mouseenter', () => {
+        colorId.style.color = 'white';
+        colorId.style.display = 'block';
+    });
+
+    hexagon.addEventListener('mouseleave', () => {
+        colorId.style.color = 'transparent';
+        colorId.style.display = 'none';
+    });
+});
+
+hexagons.forEach((hexagon, index) => {
+    hexagon.addEventListener('mouseenter', () => {
+        hexagon.querySelector('.color__id').style.display = 'block';
+    });
+
+    hexagon.addEventListener('mouseleave', () => {
+        hexagon.querySelector('.color__id').style.display = 'none';
+    });
+});
+
+hexagons.forEach((hexagon, index) => {
+    hexagon.addEventListener('mouseenter', () => {
+        const hexCode = hexagon.querySelector('.color__id').innerHTML;
+        const rgbCode = hexToRgb(hexCode);
+        hexagon.querySelector('.color__id').innerHTML = `${hexCode}<br>${rgbCode}`;
+        hexagon.querySelector('.color__id').style.display = 'block';
+    });
+
+    hexagon.addEventListener('mouseleave', () => {
+        const hexCode = hexagon.querySelector('.color__id').innerHTML.split('<br>')[0];
+        hexagon.querySelector('.color__id').innerHTML = hexCode;
+        hexagon.querySelector('.color__id').style.display = 'none';
+    });
+});
